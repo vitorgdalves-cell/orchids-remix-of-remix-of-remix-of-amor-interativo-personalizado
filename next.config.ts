5efd0e07794aeae66aa4622dfad5ad203d009e28
vitorgdalves-cell <vitorgd.alves@gmail.com>
2026-02-03 22:00:04 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
   },
